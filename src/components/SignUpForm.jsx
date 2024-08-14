b8af44e041fe808d09050c14895d7d3a61caa61c
Jessica Lafferty <jessica.a.lafferty@gmail.com>
2024-08-14 17:15:37 -0400

    commit 3

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,17 +1,41 @@
 import { useState, useEffect } from "react";
 export default function SignUpForm() {
-
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
-    
+   
     async function handleSubmit(event) {
         event.preventDefault();
-        console.log("Hello");
+    
+        try {
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                username: username,
+                password: password,
+              }),
+            });
+      
+            if (response.ok) {
+              const data = await response.json();
+              console.log("Signup successful:", data);
+             
+            } else {
+              const errorData = await response.json();
+              setError(errorData.message || "An error occurred.");
+              console.error("Signup failed:", errorData);
+            }
+        } catch (error) {
+          setError(error.message);
+        }
       }
     return (
         <div>
         <h2>Sign Up!</h2>
+        {error && <p>{error}</p>}
         <form onSubmit={handleSubmit}>
         <label>
           Username: 
